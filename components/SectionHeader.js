8757a82c220350c7e8c315b329c36e07746d3f91
mwalla210 <mlw343@drexel.edu>
2018-05-29 09:53:20 -0400

    Update SectionHeader.js
    PropTypes update

--- components/SectionHeader.js
+++ components/SectionHeader.js
@@ -2,7 +2,8 @@
 
 var React = require('react');
 var ReactNative = require('react-native');
-var {Component, PropTypes} = React;
+var {Component} = React;
+var PropTypes = require('prop-types');
 var {StyleSheet, View, Text, NativeModules} = ReactNative;
 
 var UIManager = NativeModules.UIManager;
